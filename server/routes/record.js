7c6d12db427ae1ffcc1bea63d1cf08b5c406dff5
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-03-30 22:21:25 -0500

    backend casi terminado, falta el esquema

--- server/routes/record.js
+++ server/routes/record.js
@@ -8,13 +8,13 @@ import { ObjectId } from 'mongodb'
 // We use it to define our routes.
 // The router will be added as a middleware and will take control of requests starting with path /record.
 const router = express.Router()
-
+// This section will help you get a list of all the records.
 router.get('/', async (req, res) => {
   let collection = await db.collection('records')
   let results = await collection.find({}).toArray()
   res.send(results).status(200)
 })
-
+// this section will help you get a single record by id.
 router.get('/:id', async (req, res) => {
   let collection = await db.collection('records')
   let query = { _id: ObjectId(req.params.id) }
@@ -22,6 +22,53 @@ router.get('/:id', async (req, res) => {
 
   if (!result) res.send('Not found').status(404)
   else res.send(result).status(200)
+})
+// This section will help you create a new record.
+router.post('/', async (req, res) => {
+  try {
+    let newDocument = {
+      name: req.body.name,
+      position: req.body.position,
+      level: req.body.level,
+    }
+    let collection = await db.collection('records')
+    let result = await collection.insertOne(newDocument)
+    res.send(result).status(200)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error adding record')
+  }
+})
+// This section will help you update a record by id.
+router.put('/:id', async (req, res) => {
+  try {
+    const query = { _id: ObjectId(req.params.id) }
+    const updates = {
+      $set: {
+        name: req.body.name,
+        position: req.body.position,
+        level: req.body.level,
+      },
+    }
+    let collection = await db.collection('records')
+    let result = await collection.updateOne(query, updates)
+    res.send(result).status(200)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error updating record')
+  }
+})
+// This section will help you delete a record
+router.delete('/:id', async (req, res) => {
+  try {
+    const query = { _id: ObjectId(req.params.id) }
+    const collection = await db.collection('records')
+    let result = await collection.deleteOne(query)
+    res.send(result).status(200)
+  } catch (error) {
+    console.log(error)
+    res.status(500).send('Error deleting record')
+  }
 })
 
 export default router
