f5b56c535f2d3c294629ec1cac3a9617c7d0edd5
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-04-01 20:32:32 -0500

    dd

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -16,13 +16,15 @@ const Record = (props) => {
       <td className='px-4 py-2 text-left align-middle [&amp;:has([role:checkbox])]:pr-0'>
         <div className='flex gap-2'>
           <Link
-            className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3'
+            className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9
+            rounded-md px-3'
             to={`/edit/${props.record._id}`}
           >
             Edit
           </Link>
           <button
-            className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9 rounded-md px-3'
+            className='inline-flex items-center justify-center whitespace-nowrap text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-slate-100 h-9
+            rounded-md px-3'
             color='red'
             type='button'
             onClick={() => {
