3e69740b79c2c49849162670dd208a79834349db
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-04-01 20:39:21 -0500

    dfdfas

--- client/src/components/RecordList.jsx
+++ client/src/components/RecordList.jsx
@@ -125,7 +125,8 @@ export default function RecordList() {
         <div className='relative w-full overflow-auto'>
           <table className='w-full caption-bottom overflow-auto'>
             <thead className='[&amp;_tr]:border-b'>
-              <tr className='border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted'>
+              <tr className='border-b transition-colors hover:bg-muted/50
+              data-[state=selected]:bg-muted'>
                 <th className='h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role:checkbox])]:pr-0'>
                   Name
                 </th>
