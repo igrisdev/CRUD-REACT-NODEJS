c7c31a80faeaa4165419ad233b93cb50ea59cf10
TJIGRIS27m <johanalvarezzz237@gmail.com>
2024-04-01 20:05:55 -0500

    ff

--- server/db/connection.js
+++ server/db/connection.js
@@ -13,10 +13,9 @@ const client = new MongoClient(uri, {
 })
 
 try {
-  // Connect the client to the server
+  // conexión a la base de datos
   await client.connect()
 
-  // Send a ping to confirm a successful connection
   await client.db('admin').command({ ping: 1 })
   console.log('Pinged your deployment. You successfully connected to MongoDB!')
 } catch (err) {
